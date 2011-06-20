4a97f765b74f5822192a8960fa2c2db6025d9ab6
Samuel Clay <samuel@ofbrooklyn.com>
2011-06-20 14:04:31 -0400

    Removing search button.

--- build/visualsearch.js
+++ build/visualsearch.js
@@ -21,6 +21,9 @@
   if (!VS.model)  VS.model  = {};
   if (!VS.utils)  VS.utils  = {};
   
+  // Use namespaced jQuery.
+  var $ = jQuery;
+  
   // Entry-point used to tie all parts of VisualSearch together. It will either attach
   // itself to `options.container`, or pass back the `searchBox` so it can be rendered
   // at will.
@@ -69,8 +72,7 @@ VS.ui.SearchBox = Backbone.View.extend({
   events : {
     'click .cancel_search_box'  : 'clearSearch',
     'mousedown .VS-search-box'  : 'focusSearch',
-    'dblclick .VS-search-box'   : 'highlightSearch',
-    'click #search_button'      : 'searchEvent'
+    'dblclick .VS-search-box'   : 'highlightSearch'
   },
 
   // Creating a new SearchBox registers handlers for re-rendering facets when necessary,
@@ -1472,7 +1474,7 @@ VS.model.SearchQuery = Backbone.Collection.extend({
 })();(function(){
 window.JST = window.JST || {};
 
-window.JST['search_box'] = _.template('<div class="VS-search">  <div id="search_container">    <div id="search_button" class="minibutton">Search</div>    <div id="search_box_wrapper" class="VS-search-box">      <div class="icon search_glyph"></div>      <div class="search_inner"></div>      <div class="icon cancel_search cancel_search_box" title="clear search"></div>    </div>  </div></div>');
+window.JST['search_box'] = _.template('<div class="VS-search">  <div id="search_container">    <div id="search_box_wrapper" class="VS-search-box">      <div class="icon search_glyph"></div>      <div class="search_inner"></div>      <div class="icon cancel_search cancel_search_box" title="clear search"></div>    </div>  </div></div>');
 window.JST['search_facet'] = _.template('<% if (model.has(\'category\')) { %>  <div class="category"><%= model.get(\'category\') %>:</div><% } %><div class="search_facet_input_container">  <input type="text" class="search_facet_input VS-interface" value="" /></div><div class="search_facet_remove icon cancel_search"></div>');
 window.JST['search_input'] = _.template('<input class="search_box" type="text" />');
 })();
